64637fccf7f648bf278082f7421e1ab8a2abfb0f
Emily <emily.zy.lin@gmail.com>
2024-06-30 23:29:26 -0500

    add new key value pair based on prevDiary so that the value of input will be logged

--- frontend-react/src/component/Body/Diary/CreateDiary.jsx
+++ frontend-react/src/component/Body/Diary/CreateDiary.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateDiary() {
+  const [diary, setDiary] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    console.log(event);
+    const { name, value } = event.target;
+
+    //deconstructure
+    //相当于
+    //const name = event.target.name;
+    //const value = event.target.value;
+    //记住必须在set state外面assign event.target，不能在setDiary内使用（原因：synthetic event)
+
+    setDiary((prevDiary) => {
+      return {
+        ...prevDiary,
+        [name]: value,
+      };
+    });
+    //在原来state的基础上增添键值对
+  }
+
   return (
     <div>
       <form>
-        <input type="tex" name="title" placeholder="Title" />
+        <input
+          type="tex"
+          name="title"
+          onChange={handleChange}
+          placeholder="Title"
+        />
         <textarea
-          typen="text"
-          ame="content"
+          type="text"
+          name="content"
+          onChange={handleChange}
           placeholder="What I have learned today..."
           rows="3"
         />
